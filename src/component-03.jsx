3723562316af42c4f29c0a464c6fc754511b2498
GabrieleMurgia <gabriele.murgia.gm@gmail.com>
2022-07-07 18:37:07 +0200

    component-03

---
+++ src/component-03.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Hello } from "./component-02";
+
+export class App extends React.Component{
+    render(){
+        <div>
+            <Hello></Hello>
+        </div>
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import  ReactDOM  from "react-dom";
 import { Hello } from "./component-02";
+import { App } from "./component-03";
 
 const root = document.querySelector("#root");
 
 
-ReactDOM.render(<Hello></Hello>,root)
+ReactDOM.render(<App></App>,root)
