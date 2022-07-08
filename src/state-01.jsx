81966e7031582a53964f30a3998bf2440e0a6db7
GabrieleMurgia <gabriele.murgia.gm@gmail.com>
2022-07-08 23:04:49 +0200

    state-05

--- src/state-01.jsx
+++ src/state-01.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 
-export class Counter extends React.Component{
-
-    state = {
+export class CounterDisplay extends React.Component{
+       state = {
             counter:this.props.counter,
     }
     
@@ -14,9 +13,17 @@ export class Counter extends React.Component{
                 counter: this.state.counter + this.props.increment
             })
         }, this.props.interval);
-    }
+    }  
 
     render(){
         return <h1>{this.state.counter}</h1>
+    }
+}
+
+export class Counter extends React.Component{
+
+   
+    render(){
+        return <CounterDisplay counter={this.props.counter} increment={this.props.increment} interval={this.props.interval}></CounterDisplay>
     }
 }
