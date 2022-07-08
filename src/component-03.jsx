80bc74ed9e3d789b58e21dfa0a40801c0353dccc
GabrieleMurgia <gabriele.murgia.gm@gmail.com>
2022-07-08 10:03:09 +0200

    props-02

--- src/component-03.jsx
+++ src/component-03.jsx
@@ -6,7 +6,7 @@ export class App extends React.Component{
     render(){
         return <div>
                 <Hello></Hello>
-                <Welcome name="Gabriele"/>
+                <Welcome />
                  </div>
     }
 }
