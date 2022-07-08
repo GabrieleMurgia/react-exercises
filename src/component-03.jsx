6adbd5869a6459f422cca050738e8f90ce0c1a46
GabrieleMurgia <gabriele.murgia.gm@gmail.com>
2022-07-08 09:40:33 +0200

    props-01

--- src/component-03.jsx
+++ src/component-03.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Hello } from "./component-02";
+import { Welcome } from "./props-01";
 
 export class App extends React.Component{
     render(){
         return <div>
                 <Hello></Hello>
+                <Welcome name="Gabriele"/>
                  </div>
     }
 }
