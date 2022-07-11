08b9100b43eedfcf51f45a835389cf1e8ff91843
GabrieleMurgia <gabriele.murgia.gm@gmail.com>
2022-07-11 22:23:34 +0200

    forms-03

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,47 @@ export class Login extends React.Component{
     state = {
         username:"",
         password:"",
-        checkbox:"",
     }
-  
+
+    handleInputs = (event)=>{
+        
+        if(event.target.id == "username"){
+            this.setState({
+                ...this.state,
+                username: event.target.value
+            })
+        }else if(event.target.id == "password"){
+            this.setState({
+                ...this.state,
+                password: event.target.value
+            })
+        }   
+    }
     render(){
         return <div>
-
                 <label for="username">Username:</label>
-                <input for="username"></input>
+                <input id="username" onChange={this.handleInputs}></input>
                 <label for="password">Password:</label>
-                <input id="password"></input>
+                <input id="password" onChange={this.handleInputs}></input>
                 <label for="rememberCheckbox">Remember</label>
                 <input id="rememberCheckbox" type="checkbox"></input>
-                
+                <button onClick={() => this.props.onLogin(this.state)} disabled={!(this.state.password && this.state.username)}>Login</button>
                </div>
     }
 }
-
-
 export class App extends React.Component{
-    render(){
 
-        return <Login></Login>
+
+    onLogin = (state)=>{
+          const obj = {
+                username:state.username,
+                password:state.password
+            }
+            console.log(obj)
+            return obj
+    }
+    render(){
+        return <Login onLogin={this.onLogin} ></Login>
     }
 }
 
