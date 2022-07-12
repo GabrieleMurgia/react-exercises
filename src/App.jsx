955aad7784c2d493d884cc99c76d0bc345a6e774
GabrieleMurgia <gabriele.murgia.gm@gmail.com>
2022-07-12 22:39:30 +0200

    lists-03

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,47 @@
 import React from "react";
 
-
-export class UncontrolledLogin extends React.Component{
-    handleFormSubmit = (event) => {
-        event.preventDefault()
-
-      const username =  event.target.elements.username.value
-      const password =  event.target.elements.password.value
-      const remember = event.target.elements.remember.checked
-
-      console.log({
-        username,
-        password,
-        remember
+export class TodoList extends React.Component{
+    state = {
+        items: [],
+        userInput: '',
+      }
+     
+      inputHandler = (event)=>{
+      this.setState({
+        userInput:event.target.value,
       })
-    }
+      }
+      
+      submitHandler = (event)=>{
+        event.preventDefault();
+        this.setState({
+            items:[...this.state.items,this.state.userInput],
+            userInput:"",
+        })
+      }
     
-    render(){
-        return(
-            <div>
-
-                <form onSubmit={this.handleFormSubmit}>
-                <input name="username" autoFocus/>
-                <input name="password" type="password"/>
-                <input name="remember" type="checkbox"/>
-
-                <button type="submit">Login</button>
-                <button type="reset">Reset</button>
-                </form>
-            </div>
+      render() {
+        return (
+          <div>
+            <ul>
+              {
+                this.state.items.map((li,key) => <li {...{key}}>{li}</li>)
+              } 
+            </ul>
+             <form onSubmit={this.submitHandler}>
+                <input value={this.state.userInput} onChange={this.inputHandler}></input>
+                <button type="submit">Submit</button>
+             </form>
+          </div>
         )
-    }
+      }
 }
+
+
 
 export class App extends React.Component{
 
     render(){
-        return <UncontrolledLogin onLogin={this.onLogin} resetValue={this.resetValue}></UncontrolledLogin>
+        return <TodoList ></TodoList>
     }
 }
