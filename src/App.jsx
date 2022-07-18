1e0c8f93a32ef6ceeada6b6e0c88bcf05c72f560
GabrieleMurgia <gabriele.murgia.gm@gmail.com>
2022-07-18 16:05:49 +0200

    render-props-01

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,77 @@
+
 import React from "react";
+export class TodoList extends React.Component{
+    state = {
+        items: [],
+        userInput:'',
+      }
+      inputHandler = (event)=>{
+      this.setState({
+        userInput:event.target.value,
+      })
+      }
+      submitHandler = (event)=>{
+        event.preventDefault()
+        
+        this.setState({
+            items:[this.props.items.push(this.state.userInput)],
+            userInput:"",
+        })
+        console.log("ciao",this.state.userInput,this.props.items)
+      }
+      handleReset = ()=>{
+        this.setState({
+          items:[]
+        })
+      } 
+      render() {
+        return (
+          <div>
+          <ul>
+          {
+               this.props.items.map((li,key) =>  <li {...{key}}>{li} <button onClick={this.props.handleDelete}>Elimina</button></li>)
+              } 
+          </ul>
+             <form onSubmit={this.submitHandler}>
+                <input  value={this.state.userInput} onChange={this.inputHandler}></input>
+                <button type="submit">Submit</button>
+             </form>
+             <button onClick={this.handleReset}>Reset UL</button>
+          </div>
+        )
+      }
+}
 
+export class Wrapper extends React.Component{
 
-export class Container extends React.Component{
+  state = {
+    items: [],
+    userInput:'',
+  }
+  handleDelete = (event)=>{
+    let textContentLiAndButton = event.target.parentElement.textContent
+    let array = textContentLiAndButton.split(" ")
+    array.splice(-1,1)
+    let textContentLi = array.join(" ");
+    console.log(textContentLi)
+    let stateClone = [...this.state.items]
+    let index = stateClone.indexOf(textContentLi)
+    stateClone.splice(index,1)
+    console.log(stateClone)
+    this.setState({
+      items:stateClone
+    })        
+  }
+
   render(){
-    return (
-      <div className="component-class">
-      <div className="component-title">{this.props.title}</div>
-       <div className="component-body">{this.props.children}</div>
-      </div>
-    )
+    return( <div>{this.props.render(this.state.items,this.handleDelete)}</div>)
   }
 }
-export class HelloWorld extends React.Component{
-  render(){
-    return(
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    )
-  }
-}
+
 export class App extends React.Component{
     render(){
-        return <Container title="Che bel titolo">
-            <HelloWorld></HelloWorld>
-              </Container>
+        return <Wrapper render={(items,handleDelete)=> {
+          return <TodoList items={items} handleDelete={handleDelete}></TodoList>
+        }}></Wrapper>
     }
 }
